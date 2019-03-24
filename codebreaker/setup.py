cbbb3f90882251e98eb79a628d5fc5cfc28b5687
Stephen Bapple <stephen.bapple@gmail.com>
2019-03-23 22:25:10 -0600

    Changed the structure of codebreaker to conform to standard Python conventions

---
+++ codebreaker/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='codebreaker',
+    author='Stephen Bapple',
+    author_email='stephen.bapple@gmail.com',
+    description='Python version of the Codebreaker program for the RSpec book.',
+    url='https://github.com/stephen-bapple/rspec-book-in-python/codebreaker/',
+    packages=find_packages(exclude=('features')),
+    install_requires=[
+        'expects>=0.9.0',  # Needed for unit tests.
+        'behave>=1.2.6',  # Needed for features.
+        'wheel>=0.33.1',  # Needed to install this package.
+    ]
+
+)
+
