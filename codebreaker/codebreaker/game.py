0b0b33417e554b1ed4a20e916a2227c2f2eead78
Stephen Bapple <stephen.bapple@gmail.com>
2019-06-15 13:41:38 -0600

    Finished refactoring for chapter 8.

--- codebreaker/codebreaker/game.py
+++ codebreaker/codebreaker/game.py
@@ -1,3 +1,6 @@
+from codebreaker.marker import Marker
+
+
 class Game:
     def __init__(self, output):
         self._output_file = output
@@ -9,32 +12,9 @@ class Game:
         self.print('Enter guess:')
 
     def guess(self, guess):
-        self.print('+'*self.exact_match_count(guess)
-                   + '-'*self.number_match_count(guess))
-
-    def exact_match_count(self, guess):
-        exact_match_count = 0
-
-        for index in range(len(guess)):
-            if self.exact_match(guess, index):
-                exact_match_count += 1
-        return exact_match_count
-
-    def number_match_count(self, guess):
-        number_match_count = 0
-
-        for index in range(len(guess)):
-            if self.number_match(guess, index):
-                number_match_count += 1
-
-        return number_match_count
-
-    def exact_match(self, guess, index):
-        return guess[index] == self.secret[index]
-
-    def number_match(self, guess, index):
-        return (guess[index] in self.secret and not
-                self.exact_match(guess, index))
+        marker = Marker(self.secret, guess)
+        self.print('+'*marker.exact_match_count()
+                   + '-'*marker.number_match_count())
 
     def print(self, output):
         print(output, file=self._output_file)
