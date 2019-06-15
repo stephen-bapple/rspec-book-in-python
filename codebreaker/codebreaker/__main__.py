a32df7a99a9831ba4ad0970bc1c74b8d2c07eecf
Stephen Bapple <stephen.bapple@gmail.com>
2019-06-15 15:43:55 -0600

    Added a little bit to the main script for exploratory testing.

--- codebreaker/codebreaker/__main__.py
+++ codebreaker/codebreaker/__main__.py
@@ -1,14 +1,24 @@
 #! /usr/bin/env python
+import random
 from sys import stdout
 
 from codebreaker import Game
 
+
+def generate_secret_code():
+    options = list('123456')
+    return ''.join(random.choice(options) for _ in range(4))
+
+secret_code = generate_secret_code()
+
 game = Game(stdout)
-game.start('1234')
+game.start(secret_code)
 
 guess = input().strip()
 
 while guess:
     game.guess(guess)
     guess = input().strip()
+
+print(f'The secret code was: <{secret_code}>')
 
